c62efdb76eb82bba501d3634438c4468ba5e175d
Ben <b.wayne@me.com>
2018-04-01 16:34:25 -0600

    starting spotify

--- liri.js
+++ liri.js
@@ -2,16 +2,13 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Twitter = require("twitter");
 var fs = require("fs");
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var input = process.argv[2];
 var p1 = process.argv[3];
 var p2 = process.argv[4];
-
-if (input === "my-tweets") {
-  tweeter(p1);
-}
 
 function tweeter() {
   client.get(
@@ -20,13 +17,37 @@ function tweeter() {
     (error, tweets, response) => {
       if (error) console.log(error);
       else {
+        console.log("====================================================");
         console.log("@" + p1 + "'s last twenty tweets: ");
         for (let i = 0; i < tweets.length; i++) {
           var element = tweets[i];
           console.log(element.created_at, ": ", element.text);
         }
-        console.log(tweets)
+        console.log("====================================================");
       }
     }
   );
+}
+
+function test() {
+  spotify.search({ type: "track", query: p1 }, function(err, data) {
+    if (err) {
+      console.log("Error occurred: " + err);
+      return;
+    } else {
+      console.log("====================================================");
+      console.log(data);
+      console.log("====================================================");
+    }
+  });
+}
+
+switch (input) {
+  case "my-tweets":
+    tweeter();
+    break;
+
+  case "spotify-this-song":
+    test();
+    break;
 }
